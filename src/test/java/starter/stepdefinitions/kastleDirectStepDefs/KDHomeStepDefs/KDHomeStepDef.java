f1819bae95b1bec6fe19b47a6bf6e77c2332fe7a
varunlambda <108345053+varunlambda@users.noreply.github.com>
2025-04-03 02:31:03 +0530

    Update KDHomeStepDef.java

--- src/test/java/starter/stepdefinitions/kastleDirectStepDefs/KDHomeStepDefs/KDHomeStepDef.java
+++ src/test/java/starter/stepdefinitions/kastleDirectStepDefs/KDHomeStepDefs/KDHomeStepDef.java
@@ -22,6 +22,8 @@ public class KDHomeStepDef {
     public void beforeScenario(Scenario scenario) {
         String scenarioName = scenario.getName();
 
+        String ltBuildName = System.getProperty("LT_BUILD_NAME", "Kastle_WebUI_Automation_Default");
+
         System.out.println("LT_USERNAME: " + System.getProperty("LT_USERNAME"));
         System.out.println("KEY: " + System.getProperty("LT_ACCESS_KEY"));
         System.out.println("KEY: " + System.getProperty("LT_BUILD_NAME"));
@@ -30,6 +32,8 @@ public class KDHomeStepDef {
         remoteWebDriver = (RemoteWebDriver) ((DevToolsWebDriverFacade) ThucydidesWebDriverSupport.getDriver()).getProxiedDriver();
         // Update session name on LambdaTest
         remoteWebDriver.executeScript("lambda-name=" + scenarioName);
+        remoteWebDriver.executeScript("lambda-build=" + ltBuildName);
+        
     }
 
     @Given("user open {string} application")
