7eb5dbb24eb11f0e4bd0925e2b06a62220dfd088
varunlambda <108345053+varunlambda@users.noreply.github.com>
2025-04-03 13:21:56 +0530

    Update KDHomeStepDef.java

--- src/test/java/starter/stepdefinitions/kastleDirectStepDefs/KDHomeStepDefs/KDHomeStepDef.java
+++ src/test/java/starter/stepdefinitions/kastleDirectStepDefs/KDHomeStepDefs/KDHomeStepDef.java
@@ -7,8 +7,8 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.annotations.Steps;
-//import net.thucydides.core.webdriver.DevToolsWebDriverFacade;
-//import net.thucydides.core.webdriver.ThucydidesWebDriverSupport;
+import net.thucydides.core.webdriver.DevToolsWebDriverFacade;
+import net.thucydides.core.webdriver.ThucydidesWebDriverSupport;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import starter.Pages.kastleDirectPages.KDHomePage.KDHomePage;
 
@@ -28,23 +28,16 @@ public class KDHomeStepDef {
         String buildName = System.getProperty("LT_BUILD_NAME");
         System.out.println("Final Build Name: " + buildName);
 
-        System.out.println("LT_USERNAME: " + System.getProperty("LT_USERNAME"));
-        System.out.println("KEY: " + System.getProperty("LT_ACCESS_KEY"));
         System.out.println("BUILD NAME" + System.getProperty("LT_BUILD_NAME"));
 
-        String username = System.getenv("LT_USERNAME");
-        String accessKey = System.getenv("LT_ACCESS_KEY");
         String buildName1 = System.getProperty("LT_BUILD_NAME", System.getenv("LT_BUILD_NAME"));
 
-
-        System.out.println("LT_USERNAME - 1: " + username);
-        System.out.println("LT_ACCESS_KEY - 1: " + accessKey);
         System.out.println("LT_BUILD_NAME - 1: " + buildName1);
 
         // Get the current WebDriver instance
-      //  remoteWebDriver = (RemoteWebDriver) ((DevToolsWebDriverFacade) ThucydidesWebDriverSupport.getDriver()).getProxiedDriver();
+        remoteWebDriver = (RemoteWebDriver) ((DevToolsWebDriverFacade) ThucydidesWebDriverSupport.getDriver()).getProxiedDriver();
         // Update session name on LambdaTest
-    //   remoteWebDriver.executeScript("lambda-name=" + scenarioName);
+        remoteWebDriver.executeScript("lambda-name=" + scenarioName);
        // remoteWebDriver.executeScript("lambda-build=" + ltBuildName);
         
     }
